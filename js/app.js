f9b9bee7affc05f450963f7c3c1933930f9d65ce
Duran Cristhian <durancristhian@gmail.com>
2015-11-29 02:28:45 -0300

    Updates

---
+++ js/app.js
@@ -0,0 +1,16 @@
+(function () {
+  var drone;
+
+  document.addEventListener("DOMContentLoaded", function () {
+    console.log("It works!");
+    drone = document.getElementById("drone");
+    document.addEventListener("mousemove", updateDrone);
+  });
+
+  function updateDrone (event) {
+    var currentX = event.pageX - 50;
+    var currentY = event.pageY - 25;
+    drone.style.left = currentX + "px";
+    drone.style.top = currentY + "px";
+  }
+})();
